074e0289db0a89021399bd559e0ea57032a93038
yebbipurin <kyj0120@yonsei.ac.kr>
2024-10-02 15:01:01 +0900

    Update comfy_provisioning_v2_basic_imggen.sh
    added civitai toonyou

--- comfy_provisioning_v2_basic_imggen.sh
+++ comfy_provisioning_v2_basic_imggen.sh
@@ -25,6 +25,7 @@ NODES=(
     "https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes"
     "https://github.com/WASasquatch/was-node-suite-comfyui"
     "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+    "https://civitai.com/models/92834?modelVersionId=98960"
 
 )
 
